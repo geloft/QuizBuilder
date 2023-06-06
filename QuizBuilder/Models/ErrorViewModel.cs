cb101f19165cba3fd8d8100b9e8fd8120dcc3a68
Hryhoriy K <101716956+geloft@users.noreply.github.com>
2023-06-06 19:33:04 +0300

    UpdateTemplate

---
+++ QuizBuilder/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuizBuilder.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
