fc0c43ea2592520dc4987682dc4b7aaab5613a97
Hryhoriy K <101716956+geloft@users.noreply.github.com>
2023-06-07 21:21:51 +0300

    Add default template controller

---
+++ QuizBuilder/Controllers/TeacherController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QuizBuilder.Data;
+using QuizBuilder.Data.Entities;
+
+namespace QuizBuilder.Controllers
+{
+    [Authorize(Roles ="Admin, Teacher")]
+    public class TeacherController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TeacherController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Teacher
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Subjects.Include(s => s.Teacher);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Teacher/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Subjects == null)
+            {
+                return NotFound();
+            }
+
+            var subject = await _context.Subjects
+                .Include(s => s.Teacher)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            return View(subject);
+        }
+
+        // GET: Teacher/Create
+        public IActionResult Create()
+        {
+            ViewData["TeacherId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Teacher/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Password,TeacherId")] Subject subject)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(subject);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TeacherId"] = new SelectList(_context.Users, "Id", "Id", subject.TeacherId);
+            return View(subject);
+        }
+
+        // GET: Teacher/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Subjects == null)
+            {
+                return NotFound();
+            }
+
+            var subject = await _context.Subjects.FindAsync(id);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+            ViewData["TeacherId"] = new SelectList(_context.Users, "Id", "Id", subject.TeacherId);
+            return View(subject);
+        }
+
+        // POST: Teacher/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Password,TeacherId")] Subject subject)
+        {
+            if (id != subject.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(subject);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SubjectExists(subject.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TeacherId"] = new SelectList(_context.Users, "Id", "Id", subject.TeacherId);
+            return View(subject);
+        }
+
+        // GET: Teacher/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Subjects == null)
+            {
+                return NotFound();
+            }
+
+            var subject = await _context.Subjects
+                .Include(s => s.Teacher)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            return View(subject);
+        }
+
+        // POST: Teacher/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Subjects == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Subjects'  is null.");
+            }
+            var subject = await _context.Subjects.FindAsync(id);
+            if (subject != null)
+            {
+                _context.Subjects.Remove(subject);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SubjectExists(int id)
+        {
+          return (_context.Subjects?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
