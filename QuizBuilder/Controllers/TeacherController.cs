8fac15dcfc103fcacd642830b14e086b47261d31
Hryhoriy K <101716956+geloft@users.noreply.github.com>
2023-06-07 21:47:47 +0300

    Update Index and Create methods

--- QuizBuilder/Controllers/TeacherController.cs
+++ QuizBuilder/Controllers/TeacherController.cs
@@ -3,40 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using QuizBuilder.Data;
 using QuizBuilder.Data.Entities;
+using QuizBuilder.ViewModels;
 
 namespace QuizBuilder.Controllers
 {
     [Authorize(Roles ="Admin, Teacher")]
     public class TeacherController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private readonly ApplicationDbContext _dbContext;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public TeacherController(ApplicationDbContext context)
+        public TeacherController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, ApplicationDbContext dbContext)
         {
-            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _dbContext = dbContext;
         }
 
         // GET: Teacher
         public async Task<IActionResult> Index()
         {
-            var applicationDbContext = _context.Subjects.Include(s => s.Teacher);
-            return View(await applicationDbContext.ToListAsync());
+            var currentUser = await _userManager.GetUserAsync(User);
+            var teacherId = currentUser.Id;
+
+            var subjects = await _dbContext.Subjects
+                .Include(s => s.Teacher)
+                .Where(s => s.TeacherId == teacherId)
+                .ToListAsync();
+
+            return View(subjects);
         }
+
 
         // GET: Teacher/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null || _context.Subjects == null)
+            if (id == null || _dbContext.Subjects == null)
             {
                 return NotFound();
             }
 
-            var subject = await _context.Subjects
+            var subject = await _dbContext.Subjects
                 .Include(s => s.Teacher)
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (subject == null)
@@ -50,47 +64,48 @@ namespace QuizBuilder.Controllers
         // GET: Teacher/Create
         public IActionResult Create()
         {
-            ViewData["TeacherId"] = new SelectList(_context.Users, "Id", "Id");
             return View();
         }
 
         // POST: Teacher/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Password,TeacherId")] Subject subject)
+        public async Task<IActionResult> Create(SubjectCreateViewModel subjectModel)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(subject);
-                await _context.SaveChangesAsync();
+                var user = await _userManager.GetUserAsync(User);
+                var subject = new Subject
+                {
+                    Name = subjectModel.Name,
+                    Password = subjectModel.Password,
+                    TeacherId = user.Id
+                };
+                _dbContext.Add(subject);
+                await _dbContext.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["TeacherId"] = new SelectList(_context.Users, "Id", "Id", subject.TeacherId);
-            return View(subject);
+            return View("Index");
         }
 
         // GET: Teacher/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null || _context.Subjects == null)
+            if (id == null || _dbContext.Subjects == null)
             {
                 return NotFound();
             }
 
-            var subject = await _context.Subjects.FindAsync(id);
+            var subject = await _dbContext.Subjects.FindAsync(id);
             if (subject == null)
             {
                 return NotFound();
             }
-            ViewData["TeacherId"] = new SelectList(_context.Users, "Id", "Id", subject.TeacherId);
+            ViewData["TeacherId"] = new SelectList(_dbContext.Users, "Id", "Id", subject.TeacherId);
             return View(subject);
         }
 
         // POST: Teacher/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Password,TeacherId")] Subject subject)
@@ -104,8 +119,8 @@ namespace QuizBuilder.Controllers
             {
                 try
                 {
-                    _context.Update(subject);
-                    await _context.SaveChangesAsync();
+                    _dbContext.Update(subject);
+                    await _dbContext.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
@@ -120,19 +135,19 @@ namespace QuizBuilder.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["TeacherId"] = new SelectList(_context.Users, "Id", "Id", subject.TeacherId);
+            ViewData["TeacherId"] = new SelectList(_dbContext.Users, "Id", "Id", subject.TeacherId);
             return View(subject);
         }
 
         // GET: Teacher/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null || _context.Subjects == null)
+            if (id == null || _dbContext.Subjects == null)
             {
                 return NotFound();
             }
 
-            var subject = await _context.Subjects
+            var subject = await _dbContext.Subjects
                 .Include(s => s.Teacher)
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (subject == null)
@@ -148,23 +163,23 @@ namespace QuizBuilder.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.Subjects == null)
+            if (_dbContext.Subjects == null)
             {
-                return Problem("Entity set 'ApplicationDbContext.Subjects'  is null.");
+                return Problem("Entity set 'ApplicationDbdbContext.Subjects'  is null.");
             }
-            var subject = await _context.Subjects.FindAsync(id);
+            var subject = await _dbContext.Subjects.FindAsync(id);
             if (subject != null)
             {
-                _context.Subjects.Remove(subject);
+                _dbContext.Subjects.Remove(subject);
             }
             
-            await _context.SaveChangesAsync();
+            await _dbContext.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         private bool SubjectExists(int id)
         {
-          return (_context.Subjects?.Any(e => e.Id == id)).GetValueOrDefault();
+          return (_dbContext.Subjects?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
---
+++ QuizBuilder/ViewModels/SubjectCreateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Build.Framework;
+
+namespace QuizBuilder.ViewModels
+{
+    public class SubjectCreateViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
