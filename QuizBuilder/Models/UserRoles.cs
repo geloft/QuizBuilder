a579309dccfd2c5c597e24b9e6a8d0b83dd6e8f8
Hryhoriy K <101716956+geloft@users.noreply.github.com>
2023-06-07 18:40:14 +0300

    Add roles model and admin role set up for first user

---
+++ QuizBuilder/Models/UserRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace QuizBuilder.Models
+{
+    public static class UserRoles
+    {
+        public const string Role_Student = "Student";
+        public const string Role_Teacher = "Teacher";
+        public const string Role_Admin = "Admin";
+    }
+}
